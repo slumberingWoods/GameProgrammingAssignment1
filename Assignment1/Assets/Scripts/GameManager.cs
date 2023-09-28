4e2d7843c8014df64b152fbfa38e73109f699190
hallo <strong.victory.2002@gmail.com>
2023-09-28 00:27:52 -0400

    add animations

---
+++ Assignment1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+// Write down your variables here
+    public float Score=0;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+
+    public void IncrementScore()
+    {
+        // TODO Increment score logic and win condition 
+        ++Score;
+        if(Score >= 4) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
